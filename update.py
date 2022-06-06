bd90b580e9af4a4d1434224e370fc1236933a376
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2022-06-06 16:05:53 +0530

    Update update.py

--- update.py
+++ update.py
@@ -13,19 +13,19 @@ basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     handlers=[FileHandler('log.txt'), StreamHandler()],
                     level=INFO)
 
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
+CONFIG_URL = environ.get('CONFIG_URL')
 try:
-    if len(CONFIG_FILE_URL) == 0:
+    if len(CONFIG_URL) == 0:
         raise TypeError
     try:
-        res = rget(CONFIG_FILE_URL)
+        res = rget(CONFIG_URL)
         if res.status_code == 200:
             with open('config.env', 'wb+') as f:
                 f.write(res.content)
         else:
             log_error(f"Failed to download config.env {res.status_code}")
     except Exception as e:
-        log_error(f"CONFIG_FILE_URL: {e}")
+        log_error(f"CONFIG_URL: {e}")
 except:
     pass
 
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/anasty17/mirror-leech-telegram-bot"
+    UPSTREAM_REPO = "https://github.com/deadavatar/newvro"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
