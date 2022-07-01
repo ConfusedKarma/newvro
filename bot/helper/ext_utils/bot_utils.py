a9e826aa5a0f7d095a46eaad867bba4fa393b9ba
DeadAvatar <105623068+DeadAvatar@users.noreply.github.com>
2022-07-01 20:08:28 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -153,6 +153,8 @@ def get_readable_message():
                            f" | \n<b>⚙ Engine:</b> 𝐪𝐁𝐢𝐭𝐭𝐨𝐫𝐫𝐞𝐧𝐭\n<b>⛳ Leechers:</b> {download.torrent_info().num_leechs}"
                 except:
                     pass
+                chatid = str(download.message.chat.id)[4:]
+                msg += f'\n<b>𝗦𝗼𝘂𝗿𝗰𝗲: </b><a href="https://t.me/c/{chatid}/{download.message.message_id}">{download.message.from_user.first_name}</a>'
                 msg += f"\n<b>⏳ 𝗘𝗹𝗮𝗽𝘀𝗲𝗱: </b>{get_readable_time(time() - download.message.date.timestamp())}"
                 msg += f'\n<b>💀 𝐔𝐬𝐞𝐫 :</b> <a href="tg://user?id={download.message.from_user.id}">{download.message.from_user.first_name}</a> (<code>{download.message.from_user.id}</code>)'
                 msg += f"\n<b>⛔ 𝐓𝐨 𝐒𝐭𝐨𝐩 :</b> <code>/{BotCommands.CancelMirror} {download.gid()}</code>"
