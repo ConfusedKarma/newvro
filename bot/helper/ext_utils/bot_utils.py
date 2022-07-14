db8d686cb6a59fa6211b0b2ec779753045f23c99
Singh <90951660+ConfusedKarma@users.noreply.github.com>
2022-07-14 15:15:31 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -194,7 +194,8 @@ def get_readable_message():
         if STATUS_LIMIT is not None and tasks > STATUS_LIMIT:
             buttons = ButtonMaker()
             buttons.sbutton("⬅️Prev", "status pre")
-            buttons.sbutton(f"{PAGE_NO}/{pages} | <b>Tasks:</b> {tasks}\n", str(THREE))
+            buttons.sbutton(f"{PAGE_NO}/{pages}", str(THREE))
+            buttons.sbutton(f"<b>Tasks:</b> {tasks}")
             buttons.sbutton("➡️Next", "status nex")
             buttons.sbutton("Statistics", str(FOUR))
             button = InlineKeyboardMarkup(buttons.build_menu(3))
